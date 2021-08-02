781b56c520677be6f7245d94f73c5e4137731e6c
MatiasParedesF <matiasparedes@me.com>
2021-08-02 15:36:10 -0400

    Reparación consulta recursos no existentes

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ mongoClient.connect('mongodb://127.0.0.1:27017/miBanco',{ useNewUrlParser: true 
     }
 })
 app.use(cors());
+app.use('/api', api);
 app.use(function(req,res,next){
     var err=new Error('Archivo no encontrado');
     err.status=404;
     next(err);
 });
-app.use('/api', api);
 app.use(function(err,req,res,next){
     res.status(err.status||500);
     res.send(err.menssage);
